2744b80849bb472862cef178ac8e449acd73ab50
macadadi <macadadi36@gmail.com>
2021-05-29 09:17:15 +0300

    finalised  contact section

--- src/components/Resumebody.js
+++ src/components/Resumebody.js
@@ -6,30 +6,34 @@ import shows from './shows.png'
 import store from './store.png'
 import './resumebody.css'
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import EmailIcon from '@material-ui/icons/Email';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 function Resumebody() {
     return (
     
         <div className="resumebody">
     <Container >
-        <Row>
             {/* profile section */}
-        <Col md="6"  className="pic-div">
+        <Row className="profile-wrap">
+        
+        <Col md="4"className = 'profile-general' >
+          
         <img src={pic1} alt='Photo images' />
         </Col>
-        <Col   md="6">
-            <div className="name-col">
+        <Col  md="4">
+                 <diV className="profile-name">
                 <h1>Marice Kidura</h1>
                 <h4>Software Developer</h4>
                 <div>
                     <LinkedInIcon />
-                    yuu
                 </div>
-            </div>
-        
+                </diV>
+          
+
         </Col>
       </Row>
-      
       <Row>
         <Col md="6">
             {/*about me section */}
@@ -67,10 +71,32 @@ function Resumebody() {
        
       </Row>
       <Row>
-        <Col md="12"style={{backgroundColor:'pink',height:'250px'}}>Contact</Col>
+          {/*contact section */}
+        <Col md="12"style={{height:'250px'}}>
+            <div className="contact-outer-div">
+                <div className="contact-icons">
+                   <div> <EmailIcon color="primary" fontSize="large"/></div>
+                   <div><PhoneIcon color="primary" fontSize="large" /></div>
+                   <div> <LocationOnIcon color='primary' fontSize="large" /></div>
+                    
+                </div>
+                <div className="contact-def">
+                    <h4>Email</h4>
+                    <h4>Phone</h4>
+                    <h4>Location</h4>
+                </div>
+                <div className="contact-info">
+                    <p>macadadi36@gmail</p>
+                    <p>(254) 710770278</p>
+                    <p> Nairobi,kenya</p>
+
+                </div>
+
+            </div>
+        </Col>
       </Row>
       <Row>
-        <Col md="12"style={{backgroundColor:'blue',height:'250px'}}>Footer</Col>
+        <Col md="12"><center><h6> macadadi &copy; 2021</h6></center></Col>
       </Row>
       </Container>
         </div>
--- src/components/resumenav/Resumenav.js
+++ src/components/resumenav/Resumenav.js
@@ -1,39 +1,32 @@
 import React, { useState } from 'react';
 import {
-  Collapse,
+
   Navbar,
-  NavbarToggler,
+
   NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
+
   NavbarText
 } from 'reactstrap';
 
 const Resumenav = ({themeToggler}) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+
 
   return (
     
         <>
+        <diV className='navcon sticky-top' >
       <Navbar className='navcon sticky-top' light expand="md">
         <NavbarBrand className='navcon mr-auto' >Developer Resume</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar >
          <div className='mr-auto'></div>
-          <NavbarText className='navcon mr-4'> <NavLink href=""className='navcon'>Home</NavLink></NavbarText>
-          <NavbarText className='navcon mr-4'><NavLink href=""className='navcon'>Projects</NavLink></NavbarText>
-          <NavbarText className='navcon mr-4'><NavLink href=""className='navcon'>GitHub</NavLink></NavbarText>
+   
           <NavbarText className='navcon mr-4'><button onClick={themeToggler}>Switch Theme</button></NavbarText>
-        </Collapse>
      
       </Navbar>
+      {/*curved top */}
+      <div className="curved-top ">.</div>
+      </diV>
       </>
 
     
